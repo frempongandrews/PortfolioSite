85e6f98868f3ffcd198249bbbb8ab2c96cf4a4ab
Andrews Frempong <frempongandrews@yahoo.com>
2016-11-05 00:55:52 +0000

    fixed main menu disappearing on resize after click on small menu icon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,14 @@
 $(function() {
     var $mainNav = $(".nav-list");
     $(".hamburger-menu-icon").on("click", function() {
-        $mainNav.fadeToggle(300);
+        $mainNav.slideToggle(250);
     });
+
+
+    if (window.width() >= 640) {
+        $mainNav.show();
+    } else {
+        $mainNav.hide();
+    }
+
 });
